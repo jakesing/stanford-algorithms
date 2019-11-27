8b22c2d6d9194cd0eaec315d4c9131d263e42967
Jake <jacob.b.singer@gmail.com>
2019-11-26 20:26:17 -0500

    Update karatsubaMult.js

--- karatsubaMult.js
+++ karatsubaMult.js
@@ -1,15 +1,7 @@
-// function twoDigits(a, b, c, d){
-//     return (Math.pow(10,2)*a*c) + (10*(a*d+b*c)) + (b*d)
-
-// }
-'use strict';
 function multiply(x, y){
-    // console.log('testing')
-    //assume you have two 4 digit numbers
-    //split left half and right half of each number
     let xLen = x.toString().length
-    //let yLen = y.toString().length
-    //let factor = 10**(xLen/2)
+
+    //split the numbers into the component pieces - left half and right half of both the left half and the right half :) 
     let b = x % (10**(xLen/2));
     let a = (x - b)/(10**(xLen/2))
     let d = y % ((10**(xLen/2)));
@@ -17,10 +9,11 @@ function multiply(x, y){
     // console.log([a,c],[a,d], [b,c], [b,d])
     // console.log(a*c,a*d,b*c, b*d)
     
-    //calculate ac recursively:
+    //recursion base case:
     if(xLen == 1){
         return x*y;
     }
+    //recursive call to calculate the Karatsuba product:
     else{
         return (multiply(a,c)*(10**xLen))+((multiply(a,d)+multiply(b,c))*(10**(xLen/ 2)))+multiply(b,d)
     }
@@ -28,8 +21,7 @@ function multiply(x, y){
 
 }
 
-//3141592653589793238462643383279502884197169399375105820974944592
-//2718281828459045235360287471352662497757247093699959574966967627
+//tests
 let test1 = 3141592653589793238462643383279502884197169399375105820974944592
 let test2 = 2718281828459045235360287471352662497757247093699959574966967627
 console.time("karatsuba")
