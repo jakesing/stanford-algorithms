db538c89bcd935523b12077b0781eae0c094af9d
jakesing <jacob.b.singer@gmail.com>
2019-12-01 20:18:07 -0500

    interim updates to array inversion code

---
+++ divideAndConquer/arrayInversions.py
@@ -0,0 +1,21 @@
+# Inversions: number of pairs (i,j) of array (A) indices with i<j and A[i] > A[j]
+# n(n-1) / 2 -- the max number of inversions in an array of length n
+
+testArray = [1, 3, 5, 2, 4, 6]
+
+print(testArray)
+count = 0
+
+# brute force algorithm - full looping
+for i in range(len(testArray)):
+    for j in range(1, len(testArray)):
+        if (i < j) and (testArray[i] > testArray[j]):
+            count += 1;
+
+
+print(count)
+
+
+# inversion with recursion - left inversion if i & j <= n/2, call it a right inversion if i & j are both > n/2
+# split inversion if i <= n/2 < j -we'll recursively calculate all of these and return the sum
+# also the base case - if len of array is 1, return 0
